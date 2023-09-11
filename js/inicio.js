8fb5eb203c1dd6e201feb755b68d543a30748aa9
Amadeo Serio <amadeoserio@gmail.com>
2023-09-11 14:36:45 -0300

    agregué imágenes y nombres reales, con precios actuales al archivo .json. Además de corregir la dirección del fetch y la imgane por si no se carga

--- js/inicio.js
+++ js/inicio.js
@@ -1,3 +1,45 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// PONER FUNCIONES ANTES DE LLAMARLA!!!!!!!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { carritoVacioAparece } from "./carrito.js";
 import { agregarCarrito } from "./carrito.js";
 import { carritoLlenoAparece } from "./carrito.js";
@@ -7,10 +49,17 @@ import { storage } from "./carrito.js";
 
 export const carritoStorage = sessionStorage.getItem("carrito");
 const cartas = document.getElementById("cartas");
+const buscador = document.getElementById("buscador");
+const varietalesFiltro = document.getElementById("varietalesFiltro");
+const precioFiltro = document.getElementById("precioFiltro");
+const malbecFooter = document.getElementById("malbecFooter");
+const syrahFooter = document.getElementById("syrahFooter");
+const cabernetFooter = document.getElementById("cabernetFooter");
 
 
 //Condición que carga o no el carrito cuando inicia la página
 if (carritoStorage) {
+
     storage();
     carritoLlenoAparece();
     botonCarritoLlenoCanvas();
@@ -21,11 +70,12 @@ if (carritoStorage) {
 
 //Clase constructora para las cartas del index
 class Cards {
-    constructor(id, nombre, varietal, precio) {
+    constructor(id, nombre, varietal, precio, img) {
         this.id = id;
         this.nombre = nombre;
         this.varietal = varietal;
         this.precio = precio;
+        this.img = img;
         this.vendido = false;
     }
 
@@ -36,27 +86,31 @@ class Cards {
 
 
 //Arreglos para la creación de cada uno de los vinos
-export const vinos = [];
+export let vinos = [];
 
+
+//Llamado a los vinos que están en la base de datos
 const traerBodega = async () => {
-    const response = await fetch("../db/productos.json")
+    const response = await fetch("./db/productos.json")
     const data = await response.json();
 
     data.forEach(item => {
 
-        const { id, nombre, varietal, precio } = item;
+        const { id, nombre, varietal, precio, img } = item;
 
-        vinos.push(new Cards(id, nombre, varietal, precio))
+        vinos.push(new Cards(id, nombre, varietal, precio, img))
+
+        localStorage.setItem("vinos", JSON.stringify(vinos))
     });
 }
 
 
-//Función que trae los vinos. Tiene un .then por si se conecta correctamente y un .catch por si falla
-traerBodega().then(() => {
 //Características e información que aparece en cada una de las cartas
-    vinos.forEach((item) => {
+const crearCards = () => {
+    cartas.innerHTML = ""
 
-        const { nombre, varietal, precio, id } = item;
+    vinos.forEach((item) => {
+        const { nombre, varietal, precio, id, img } = item;
 
         const div = document.createElement("div");
         div.className = "cartas";
@@ -64,7 +118,7 @@ traerBodega().then(() => {
         <h2>${nombre}</h2>
         <h3>${varietal}</h3>
         <p>$${precio}</p>
-        <img src="https://santicheese.com/cdn/shop/products/COLECCION-CABERNET-MALBEC-LARGE_1024x.jpg?v=1544528772">
+        <img src="${img}">
         <button id="boton${id}">  <i class="fa-solid fa-cart-plus fa-2xl"></i> Sumar al carrito</button>
         `;
         cartas.append(div);
@@ -72,18 +126,103 @@ traerBodega().then(() => {
         let boton = document.getElementById(`boton${id}`);
         boton.addEventListener("click", () => agregarCarrito(id))
     });
+}
+
+
+//Función que trae los vinos. Tiene un .then por si se conecta correctamente y un .catch por si falla
+traerBodega().then(() => {
+    crearCards();
+
 }).catch(() => {
     let div = document.createElement("div");
     div.className = "errorServidor"
     div.innerHTML = `
-    <h1>Eror al conectarse al servidor</h1>
-    <img src="../images/errorServidor.jpg" alt="error del servidor"></img>
+    <h1>Error al conectarse al servidor</h1>
+    <img src="./images/errorServidor.jpg" alt="error del servidor"></img>
     `
     cartas.append(div);
 })
 
 
+// Función del buscador
+buscador.addEventListener("keyup", (e) => {
+    const vinosBuscador = vinos.filter((vino) => vino.nombre.toLowerCase().includes(e.target.value))
+    vinos = vinosBuscador
+
+    if (e.target.value !== "") {
+        crearCards(vinosBuscador);
+        vinos = JSON.parse(localStorage.getItem("vinos"));
+    } else {
+        vinos = JSON.parse(localStorage.getItem("vinos"));
+        crearCards(vinos);
+    }
+})
 
 
+// Función del filtro por varietal
+varietalesFiltro.addEventListener("click", (e) => {
+//Parseo el arreglo de vinos para que se suba en el storage y cargue correctamente cada varietal al filtrar varias veces. 
+    vinos = JSON.parse(localStorage.getItem("vinos"));
+    const vinosVarietal = vinos.filter((vino) => vino.varietal.toLowerCase().includes(e.target.innerHTML.toLowerCase()))
+    vinos = vinosVarietal
+    //Utilizo los else if porque es la forma en la que encontré que no quedara vacío cuando le daba click al
+    //hr. Por eso el else final hace que se vuelva a parsear si miss clickeas 2 o más veces seguidas.
+    if (e.target.innerHTML === "Malbec") {
+        crearCards(vinosVarietal);
+    } else if (e.target.innerHTML === "Syrah") {
+        crearCards(vinosVarietal);
+    } else if (e.target.innerHTML === "Cabernet") {
+        crearCards(vinosVarietal);
+    } else if (e.target.innerHTML === "Todos") {
+        vinos = JSON.parse(localStorage.getItem("vinos"));
+        crearCards(vinos);
+    } else {
+        vinos = JSON.parse(localStorage.getItem("vinos"));
+    }
+})
 
 
+// Función del filtro por precio
+//Al no volver a parsear el arreglo vinos al storage este filtro se puede utilizar una vez filtrado por varietal.
+precioFiltro.addEventListener("click", (e) => {
+    const orden = e.target.innerHTML;
+    let vinosFiltradoPrecio;
+
+    if (orden === "Ascendente") {
+        vinosFiltradoPrecio = vinos.sort((a, b) => a.precio - b.precio)
+    } else if (orden === "Descendente") {
+        vinosFiltradoPrecio = vinos.sort((a, b) => b.precio - a.precio)
+    }
+    crearCards(vinosFiltradoPrecio)
+});
+
+
+//Filtros del footer
+malbecFooter.addEventListener("click", (e) => { 
+        vinos = JSON.parse(localStorage.getItem("vinos"));
+        const vinosVarietal = vinos.filter((vino) => vino.varietal.toLowerCase().includes(e.target.innerHTML.toLowerCase()))
+        vinos = vinosVarietal
+        if (e.target.innerHTML === "Malbec") {
+            crearCards(vinosVarietal);
+        }
+    });
+
+
+syrahFooter.addEventListener("click", (e) => { 
+        vinos = JSON.parse(localStorage.getItem("vinos"));
+        const vinosVarietal = vinos.filter((vino) => vino.varietal.toLowerCase().includes(e.target.innerHTML.toLowerCase()))
+        vinos = vinosVarietal
+        if (e.target.innerHTML === "Syrah") {
+            crearCards(vinosVarietal);
+        }
+    });
+
+
+cabernetFooter.addEventListener("click", (e) => { 
+        vinos = JSON.parse(localStorage.getItem("vinos"));
+        const vinosVarietal = vinos.filter((vino) => vino.varietal.toLowerCase().includes(e.target.innerHTML.toLowerCase()))
+        vinos = vinosVarietal
+        if (e.target.innerHTML === "Cabernet") {
+            crearCards(vinosVarietal);
+        }
+    });
